c076e0b7f355b42f83bbefc67c160f079e9e44c0
Mauri <mauri.monzon91@gmail.com>
2024-11-19 10:21:10 -0300

    Colores personalizados

--- app/index.tsx
+++ app/index.tsx
@@ -8,10 +8,16 @@ const App = () => {
         <Text style={{ fontFamily: "WorkSans-Black" }} className="text-5xl ">
           Hola mundo!!!
         </Text>
-        <Text className="text-4xl font-work-black">Hola mundo!!!</Text>
-        <Text className="text-3xl font-work-medium ">Hola mundo!!!</Text>
-        <Text className="text-2xl font-work-light ">Hola mundo!!!</Text>
-        <Text className="text-xl">Hola mundo!!!</Text>
+        <Text className="text-4xl font-work-black text-primary">
+          Hola mundo!!!
+        </Text>
+        <Text className="text-3xl font-work-medium text-secondary">
+          Hola mundo!!!
+        </Text>
+        <Text className="text-2xl font-work-light text-secondary-100 ">
+          Hola mundo!!!
+        </Text>
+        <Text className="text-xl text-terciary">Hola mundo!!!</Text>
       </View>
     </SafeAreaView>
   );
