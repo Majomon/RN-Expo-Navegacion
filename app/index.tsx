af2be615421c3cef458800ae131be4d698ae337f
Mauri <mauri.monzon91@gmail.com>
2024-11-19 10:36:06 -0300

    Navegacion entre pantallas

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,30 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { Link, Redirect } from "expo-router";
 import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View className="mt-6 mx-2.5">
-        <Text style={{ fontFamily: "WorkSans-Black" }} className="text-5xl ">
-          Hola mundo!!!
-        </Text>
-        <Text className="text-4xl font-work-black text-primary">
-          Hola mundo!!!
-        </Text>
-        <Text className="text-3xl font-work-medium text-secondary">
-          Hola mundo!!!
-        </Text>
-        <Text className="text-2xl font-work-light text-secondary-100 ">
-          Hola mundo!!!
-        </Text>
-        <Text className="text-xl text-terciary">Hola mundo!!!</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <Redirect href={"/home"} />;
+  // return (
+  //   <SafeAreaView>
+  //     <View className="mt-6 mx-2.5">
+  //       <Text style={{ fontFamily: "WorkSans-Black" }} className="text-5xl ">
+  //         Hola mundo!!!
+  //       </Text>
+  //       <Text className="text-4xl font-work-black text-primary">
+  //         Hola mundo!!!
+  //       </Text>
+  //       <Text className="text-3xl font-work-medium text-secondary">
+  //         Hola mundo!!!
+  //       </Text>
+  //       <Text className="text-2xl font-work-light text-secondary-100 ">
+  //         Hola mundo!!!
+  //       </Text>
+  //       <Text className="text-xl text-terciary">Hola mundo!!!</Text>
+
+  //       <Link href={"/productsindex"}>Productos</Link>
+  //     </View>
+  //   </SafeAreaView>
+  // );
 };
 
 export default App;
