b489e0dc86475c6a7cc3cc1eb4afee84ae69c79f
Mauri <mauri.monzon91@gmail.com>
2024-11-19 12:56:16 -0300

    Personalizando

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,14 +1,27 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
 
 const DrawerLayout = () => {
   return (
-    <Drawer>
+    <Drawer
+      screenOptions={{
+        overlayColor: "rgba(0,0,0,0.4) ",
+        drawerActiveTintColor: "indigo",
+        headerShadowVisible: false,
+        sceneStyle: {
+          backgroundColor: "white",
+        },
+      }}
+    >
       <Drawer.Screen
         name="user/index"
         options={{
           drawerLabel: "User",
           title: "Usuario",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
         }}
       />
       <Drawer.Screen
@@ -16,6 +29,9 @@ const DrawerLayout = () => {
         options={{
           drawerLabel: "Horario",
           title: "Horario",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="calendar-outline" size={size} color={color} />
+          ),
         }}
       />
     </Drawer>
