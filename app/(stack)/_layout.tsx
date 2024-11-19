e37ec677956adaf64025c4a6002219baaa6ed90d
Mauri <mauri.monzon91@gmail.com>
2024-11-19 11:23:09 -0300

    Personalizacion del stack

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -4,11 +4,25 @@ import { Stack } from "expo-router";
 
 const StackLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        // headerShown: false,
+        headerShadowVisible: false,
+        contentStyle: {
+          backgroundColor: "white",
+        },
+      }}
+    >
       <Stack.Screen name="home/index" options={{ title: "Inicio Screen" }} />
-      <Stack.Screen name="products/index" options={{ title: "Productos Screen" }} />
+      <Stack.Screen
+        name="products/index"
+        options={{ title: "Productos Screen", animation: "ios_from_right" }}
+      />
       <Stack.Screen name="profile/index" options={{ title: "Perfil Screen" }} />
-      <Stack.Screen name="settings/index" options={{ title: "Ajustes Screen" }} />
+      <Stack.Screen
+        name="settings/index"
+        options={{ title: "Ajustes Screen" }}
+      />
     </Stack>
   );
 };
